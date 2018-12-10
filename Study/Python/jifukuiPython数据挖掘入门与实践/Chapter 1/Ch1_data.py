68ea4fd0258de0bc6e7da0e631b583735a258610
jifukui <805013833@qq.com>
2018-12-10 13:10:03 +0800

    添加移除密码的python数据挖掘

--- Study/Python/jifukuiPython数据挖掘入门与实践/Chapter 1/Ch1_data.py
+++ Study/Python/jifukuiPython数据挖掘入门与实践/Chapter 1/Ch1_data.py
@@ -1,4 +1,27 @@
 import numpy as np
 data_filename="affinity_dataset.txt"
-X=np.loadtxt(data_filename)
-print(X)
+X=np.loadtxt(data_filename) #加载TXT文件
+print (X[:5])#打印0~5的内容
+num_apple_purchases=0
+for sample in X:
+	if sample[3]==1:
+		num_apple_purchases+=1
+print("{0} people bought Apples".format(num_apple_purchases))
+from collections import defaultdict
+valid_rules=defaultdict(int)
+invalid_rules=defaultdict(int)
+num_occurances=defaultdict(int)
+for sample in X:
+	for premise in range(4):
+		if sample[premise]==0:
+			continue
+		num_occurances[premise]+=1
+		for conclusion in range (n_features):
+			if(premise==conclusion):
+				continue
+			if sample[conclusion]==1:
+				valid_rules[(premise,conclusion)]+=1
+			else
+				invalid_rules[(premise,conclusion)]+=1
+		support =valid_rules
+		
