15ef0b0c55704597f2988d7d2726bef21cef3259
jifukui <805013833@qq.com>
2018-12-12 13:43:17 +0800

    添加thttpd源码学习

---
+++ Study/sourcecode/thttpd/version.h
@@ -0,0 +1,10 @@
+/* version.h - version defines for thttpd and libhttpd */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+/*定义服务器的软件名*/
+#define SERVER_SOFTWARE "thttpd/2.29 23May2018"
+/*定义服务器的网址*/
+#define SERVER_ADDRESS "http://www.acme.com/software/thttpd/"
+
+#endif /* _VERSION_H_ */
