03331220c2b679101dac8fa2a06a573980de9879
jifukui <805013833@qq.com>
2018-12-14 13:46:05 +0800

    添加linux源码学习

--- Study/sourcecode/linux-0.11/lib/open.c
+++ Study/sourcecode/linux-0.11/lib/open.c
@@ -7,7 +7,11 @@
 #define __LIBRARY__
 #include <unistd.h>
 #include <stdarg.h>
-
+/**
+ * 打开文件
+ * filename是文件名
+ * flag是打开类型
+ * 剩下的是打开模式*/
 int open(const char * filename, int flag, ...)
 {
 	register int res;
@@ -15,11 +19,13 @@ int open(const char * filename, int flag, ...)
 
 	va_start(arg,flag);
 	__asm__("int $0x80"
-		:"=a" (res)
-		:"0" (__NR_open),"b" (filename),"c" (flag),
-		"d" (va_arg(arg,int)));
+		:"=a" (res)										//output
+		:"0" (__NR_open),"b" (filename),"c" (flag),		//input
+		"d" (va_arg(arg,int)));							//被修改的寄存器
 	if (res>=0)
+	{
 		return res;
+	}
 	errno = -res;
 	return -1;
 }
